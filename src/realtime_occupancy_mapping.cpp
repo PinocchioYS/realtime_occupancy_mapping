d21ba92ef298987f11dd75b73a2154e4bd48f386
yskwon <tea984811@gmail.com>
2025-01-10 11:44:51 +0900

    Support the recent version of SuperRay library

--- src/realtime_occupancy_mapping.cpp
+++ src/realtime_occupancy_mapping.cpp
@@ -10,38 +10,53 @@
 #include <pcl_ros/transforms.h>
 #include <pcl_ros/point_cloud.h>
 
-#include <gridmap3D_cullingregion/CullingRegionGrid3D.h>
-
-#define VIS_OCCUPIED_CELLS
+#include <superray_gridmap3d/CullingRegionGrid3D.h>
 
 class MappingServer {
 public:
-    MappingServer() : nh() {
-        // Parameters
-        nh.getParam("fixed_frame_id", FIXED_FRAME_ID);
-        nh.getParam("resolution", RESOLUTION);
-        nh.getParam("max_range", MAXIMUM_RANGE);
-        nh.getParam("hit_prob", HIT_PROB);
-        nh.getParam("miss_prob", MISS_PROB);
-        HIT_PROB  = std::max(0.0, std::min(HIT_PROB,  1.0));
-        MISS_PROB = std::max(0.0, std::min(MISS_PROB, 1.0));
+    MappingServer() : occupancy_gridmap(nullptr) { }
+
+    ~MappingServer() {
+        delete pointcloud_subscriber;
+        delete occupancy_gridmap;
+    }
+
+    bool initialize(bool verbose = true) {
+        // Initialize parameters
+        if(!nh.getParam("resolution", RESOLUTION))          RESOLUTION = 0.1;       // defalut value: 0.1 [m]
+        if(!nh.getParam("max_range", MAXIMUM_RANGE))        MAXIMUM_RANGE = 100.0;  // defalut value: 100 [m]
+        if(!nh.getParam("hit_prob", HIT_PROB))              HIT_PROB = 0.7;         // defalut value: 0.7
+        if(!nh.getParam("miss_prob", MISS_PROB))            MISS_PROB = 0.4;        // defalut value: 0.4
+        if(!nh.getParam("fixed_frame_id", FIXED_FRAME_ID))  FIXED_FRAME_ID = "map"; // defalut value: map
+
+        if(RESOLUTION < 10e-5)                  { ROS_ERROR_STREAM("Invalid voxel size: " << RESOLUTION << " [m] < 0"); return false; }
+        if(MAXIMUM_RANGE < 10e-5)               { ROS_ERROR_STREAM("Invalid maximum range: " << MAXIMUM_RANGE << " [m] < 0"); return false; }
+        if(HIT_PROB < 0.0 || HIT_PROB > 1.0)    { ROS_ERROR_STREAM("Invalid hit prob.: " << HIT_PROB << (HIT_PROB < 0.0 ? " < 0" : " > 1")); return false; }
+        if(MISS_PROB < 0.0 || MISS_PROB > 1.0)  { ROS_ERROR_STREAM("Invalid miss prob.: " << MISS_PROB << (MISS_PROB < 0.0 ? " < 0" : " > 1")); return false; }
+        if(HIT_PROB < MISS_PROB)                { ROS_ERROR_STREAM("Invalid hit/miss prob.: " << HIT_PROB << " < " << MISS_PROB); return false; }
+        if(FIXED_FRAME_ID.empty())              { FIXED_FRAME_ID = "map"; }
+
+        if(verbose) {
+            ROS_INFO_STREAM("MappingServer Parameters");
+            ROS_INFO_STREAM("   " << "Voxel size: " << RESOLUTION << " [m]");
+            ROS_INFO_STREAM("   " << "Max. range: " << MAXIMUM_RANGE << " [m]");
+            ROS_INFO_STREAM("   " << "Hit Prob. : " << HIT_PROB);
+            ROS_INFO_STREAM("   " << "Miss Prob.: " << MISS_PROB);
+        }
+
+        // Create a grid map
+        occupancy_gridmap = new gridmap3d::CullingRegionGrid3D(RESOLUTION);
+        occupancy_gridmap->setProbHit(HIT_PROB);
+        occupancy_gridmap->setProbMiss(MISS_PROB);
 
         pointcloud_subscriber = new message_filters::Subscriber<sensor_msgs::PointCloud2>(nh, "/realtime_occupancy_mapping/pointcloud_in", 1);
         tf_pointcloud_subscriber = new tf::MessageFilter<sensor_msgs::PointCloud2>(*pointcloud_subscriber, tf_listener, FIXED_FRAME_ID, 1);
         tf_pointcloud_subscriber->registerCallback(boost::bind(&MappingServer::update_occupancy_map, this, _1));
 
-#ifdef VIS_OCCUPIED_CELLS
+        // Visualization Publishers
         occupied_cells_publisher = nh.advertise<sensor_msgs::PointCloud2>("/realtime_occupancy_mapping/occupied_cells", 1);
-#endif
 
-        occupancy_gridmap = new gridmap3D::CullingRegionGrid3D(RESOLUTION);
-        occupancy_gridmap->setProbHit(HIT_PROB);
-        occupancy_gridmap->setProbMiss(MISS_PROB);
-    }
-    ~MappingServer() {
-        delete pointcloud_subscriber;
-
-        delete occupancy_gridmap;
+        return true;
     }
 
     /*
@@ -52,8 +67,8 @@ public:
      * @param src_pc: a pointcloud in the sensor coordinate
      */
     void update_occupancy_map(const sensor_msgs::PointCloud2ConstPtr& _src_pc) {
-        gridmap3D::Pointcloud pointcloud;
-        gridmap3D::point3d origin;
+        gridmap3d::Pointcloud pointcloud;
+        gridmap3d::point3d origin;
         if(!parse_valid_sensor_measurement(*_src_pc, pointcloud, origin)) {
             ROS_ERROR_STREAM("Invalid sensor measurement.");
             return;
@@ -61,10 +76,8 @@ public:
 
         occupancy_gridmap->insertPointCloudRays(pointcloud, origin);
 
-#ifdef VIS_OCCUPIED_CELLS
         if(occupied_cells_publisher.getNumSubscribers() > 0)
             publish_occupied_cells();
-#endif
     }
 
     /*
@@ -77,8 +90,8 @@ public:
         pcl::PointCloud<pcl::PointXYZ> pcl_pointcloud;
         for(const auto& node : *(occupancy_gridmap->getGrid())) {
             if(occupancy_gridmap->isNodeOccupied(node.second)) {
-                gridmap3D::point3d center = occupancy_gridmap->keyToCoord(node.first);
-                pcl_pointcloud.push_back(pcl::PointXYZ(center.x(), center.y(), center.z()));
+                gridmap3d::point3d center = occupancy_gridmap->keyToCoord(node.first);
+                pcl_pointcloud.emplace_back(center.x(), center.y(), center.z());
             }
         }
 
@@ -97,26 +110,24 @@ public:
 protected:
     // Node handle
     ros::NodeHandle nh;
+
     // Transform and pointcloud subscribers
     message_filters::Subscriber<sensor_msgs::PointCloud2>*  pointcloud_subscriber;
     tf::MessageFilter<sensor_msgs::PointCloud2>*            tf_pointcloud_subscriber;
     tf::TransformListener                                   tf_listener;
 
     // Options =========================================================================================================
-protected:
-    std::string  FIXED_FRAME_ID;
+    std::string FIXED_FRAME_ID;
     double RESOLUTION;
     double MAXIMUM_RANGE;
     double HIT_PROB;
     double MISS_PROB;
 
-#ifdef VIS_OCCUPIED_CELLS
     // Map publisher
     ros::Publisher occupied_cells_publisher;
-#endif
 
     // Occupancy grid
-    gridmap3D::CullingRegionGrid3D* occupancy_gridmap;
+    gridmap3d::CullingRegionGrid3D* occupancy_gridmap;
 
     /*
      * Parse the valid points in the sensor measurement
@@ -127,7 +138,7 @@ protected:
      * @param origin: [output] sensor origin in the world coordinate
      * @return validity of the sensor measurement
      */
-    bool parse_valid_sensor_measurement(const sensor_msgs::PointCloud2& _ros_pc, gridmap3D::Pointcloud& _pc, gridmap3D::point3d& _origin) {
+    bool parse_valid_sensor_measurement(const sensor_msgs::PointCloud2& _ros_pc, gridmap3d::Pointcloud& _pc, gridmap3d::point3d& _origin) {
         // Pose of the sensor frame
         tf::StampedTransform sensor_to_world;
         try{
@@ -162,9 +173,9 @@ protected:
         pcl::PointCloud<pcl::PointXYZ> pcl_pointcloud_in_world_coordinate;
         pcl::transformPointCloud(pcl_pointcloud_in_sensor_coordinate, pcl_pointcloud_in_world_coordinate, transform);
 
-        for(const auto& point : pcl_pointcloud_in_world_coordinate) {
+        _pc.reserve(pcl_pointcloud_in_world_coordinate.size());
+        for(const auto& point : pcl_pointcloud_in_world_coordinate)
             _pc.push_back(point.x, point.y, point.z);
-        }
 
         // sensor origin
         _origin.x() = sensor_to_world.getOrigin().x();
@@ -179,17 +190,19 @@ protected:
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "realtime_occupancy_mapping");
-    ros::NodeHandle nh;
 
-    MappingServer mapping_server;
+    std::shared_ptr<MappingServer> mapping_server = std::make_shared<MappingServer>();
 
-    try{
-        ros::spin();
+    bool success = mapping_server->initialize(true);
+    if(success) {
+        try{
+            ros::spin();
+        }
+        catch(std::runtime_error& e) {
+            ROS_ERROR("Exception: %s", e.what());
+            return -1;
+        }
     }
-    catch(std::runtime_error& e) {
-        ROS_ERROR("Exception: %s", e.what());
-        return -1;
-    }
-
+    
     return 0;
 }
